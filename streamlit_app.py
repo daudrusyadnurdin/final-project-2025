16e4585ca2f4a590c5a5bc1ea1ebe5955f7a186e
daudrusyadnurdin <49769264+daudrusyadnurdin@users.noreply.github.com>
2025-10-07 00:05:37 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np #number
 
-import seaborn as sns
+# import seaborn as sns
 import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
